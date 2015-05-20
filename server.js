121c0606652881ec8e5176e1f3819393baf8319b
Kravchuk Igor <kraigo@live.com>
2015-05-20 12:18:48 +0300

    rhcoud

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     path = require('path');
 
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app = express();
 game = require('./game');
 app.use(express.static('public'));
 
-var server = require('http').createServer(app).listen(80);
+var server = require('http').createServer(app).listen(server_port, server_ip_address);
 var io = require('socket.io').listen(server);
 
 
