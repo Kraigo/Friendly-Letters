116d61b8a23e28eea53df8abf259c14ab524a3e6
Kravchuk Igor <kraigo@live.com>
2015-05-24 17:18:50 +0300

    fix animation

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ game = require('./game');
 app.use(express.static('public'));
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP
 
 var server = require('http').createServer(app).listen(server_port, server_ip_address);
 var io = require('socket.io').listen(server);
